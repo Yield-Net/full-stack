3b730dd16f7a17b7f151a27cd122cff58fc28361
ZMCodi Mac <zaim.mohsin@gmail.com>
2025-04-13 10:57:54 +0100

    fix backend path

--- frontend/src/api/core/OpenAPI.ts
+++ frontend/src/api/core/OpenAPI.ts
@@ -20,7 +20,7 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-    BASE: 'https://full-stack-indol-six.vercel.app',
+    BASE: 'https://yield-net-backend.onrender.com',
     VERSION: '0.1.0',
     WITH_CREDENTIALS: false,
     CREDENTIALS: 'include',
